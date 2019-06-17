ea8781f1878ea0c5bf7339f382c2aea532fe6a59
longchong <116294>
2019-06-17 17:09:24 +0800

    init

--- src/main/java/com/example/demo/lcController.java
+++ src/main/java/com/example/demo/lcController.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class lcController {
     public static void main(String[] args) {
-        System.out.println("hehe");
+        System.out.println("hehexxxx");
     }
 }
